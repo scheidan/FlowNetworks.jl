7aee449d48cd120ff1eef58da67be58df1a65476
Andreas Scheidegger <andreas.scheidegger@eawag.ch>
2014-11-18 14:18:12 +0100

    add test for dist on same segment

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ FlowNetworks
 ==============
 
 [![Build Status](https://travis-ci.org/scheidan/FlowNetworks.jl.svg?branch=master)](https://travis-ci.org/scheidan/FlowNetworks.jl)
-
 [![Coverage Status](https://img.shields.io/coveralls/scheidan/FlowNetworks.jl.svg)](https://coveralls.io/r/scheidan/FlowNetworks.jl?branch=master)
 
 Types and function to represent hydrological flow networks.
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,6 @@ import Graphs
 
 g = init_flow_net("Outflow")
 
-
 add_segment!(g, Graphs.vertices(g)[1], "A",  200.0)
 add_segment!(g, "A", "B", 50.0)
 add_segment!(g, "A", "C", 20.0)
@@ -22,6 +21,7 @@ lo1 = Location(33.0, "Outflow", "A", g)
 lo2 = Location(22.2, Graphs.edges(g)[2])
 lo3a = Location(3.3, "A", "C", g)
 lo3b = Location(3.3, 3, g)
+lo4 = Location(9.9, "A", "C", g)
 
 ## illegal Locations
 @test_throws ErrorException Location(3333.0, Graphs.edges(g)[1])
@@ -39,12 +39,14 @@ v = Graphs.vertices(g)
 @test is_flowconneted(g, v[3], v[4]) == false
 @test is_flowconneted(g, v[4], v[3]) == false
 
-@test dist(g, lo1, lo1) == 0.0
-@test dist(g, lo1, lo2) == 189.2
-@test dist(g, lo1, lo3a) == 170.3
-@test dist(g, lo2, lo3a) == 0.0
-@test dist(g, lo1, lo3a) == dist(g, lo1, lo3b)
-@test dist(g, lo1, lo2) == dist(g, lo2, lo1)
+@test_approx_eq dist(g, lo1, lo1) 0.0
+@test_approx_eq dist(g, lo1, lo2) 189.2
+@test_approx_eq dist(g, lo1, lo3a) 170.3
+@test_approx_eq dist(g, lo2, lo3a) 0.0
+@test_approx_eq dist(g, lo3a, lo4) 6.6
+@test_approx_eq dist(g, lo1, lo3a) dist(g, lo1, lo3b)
+@test_approx_eq dist(g, lo1, lo2) dist(g, lo2, lo1)
+
 
 ## --- misc
 
